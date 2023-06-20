3a4baf250086086af1a34595283c3fb258953170
Renato Ide Ogata <renatoogata95@gmail.com>
2023-06-19 21:55:04 -0300

    Stack navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
 import { Baloo2_700Bold } from '@expo-google-fonts/baloo-2'
 
-import { Home } from '@screens/Home';
 import { Loading } from '@components/Loading';
+import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold, Baloo2_700Bold })
 
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
-    !fontsLoaded ? <Home /> : <Loading />
+    <Routes />
   );
 }
