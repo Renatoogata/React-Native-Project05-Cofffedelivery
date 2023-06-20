8a1d2df0784dad85fdf98ce89041f21119d893f9
Renato Ide Ogata <renatoogata95@gmail.com>
2023-06-19 21:36:58 -0300

    babel, assets, google fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
+import { Baloo2_700Bold } from '@expo-google-fonts/baloo-2'
+
+import { Home } from '@screens/Home';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold, Baloo2_700Bold })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    !fontsLoaded ? <Home /> : <Loading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/theme.ts
@@ -0,0 +1,50 @@
+export const THEME = {
+  COLORS: {
+    WHITE: "#FFFFFF",
+
+    GRAY_100: '#272221',
+    GRAY_200: '#403937',
+    GRAY_300: '#574F4D',
+    GRAY_400: '#8D8686',
+    GRAY_500: '#D7D5D5',
+    GRAY_600: '#E6E5E5',
+    GRAY_700: '#EDEDDE',
+    GRAY_800: '#F3F2F2',
+    GRAY_900: '#FAFAFA',
+
+    YELLOW: "#DBAC2C",
+    YELLOW_DARK: "#C47F17",
+
+    PURPLE_LIGHT: '#EBE5F9',
+    PURPLE: '#8047F8',
+    PURPLE_DARK: '#4B2995',
+
+    RED_LIGHT: '#F2DFD8',
+    RED: '#E8BAAB',
+    RED_DARK: '#C44117',
+  },
+
+  FONTS: {
+    ROBOTO_REGULAR: "Roboto_400Regular",
+    ROBOTO_BOLD: "Roboto_700Bold",
+    BALOO2_BOLD: 'Baloo2_700Bold',
+  },
+
+  SIZES: {
+    ROBOTO: {
+      XS: 12,
+      SM: 14,
+      MD: 16,
+      LG: 20,
+      TAG: 10,
+      BUTTON: 14,
+    },
+    BALOO2: {
+      XS: 14,
+      SM: 16,
+      MD: 20,
+      LG: 24,
+      XL: 36,
+    }
+  }
+} as const;
